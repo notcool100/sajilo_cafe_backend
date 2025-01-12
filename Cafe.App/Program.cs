a6149aa2846b9b88fb22c57dd64ba0e1ab8e0b13
Anjal Joshi <anjaljoshi6@gmail.com>
2025-01-12 21:24:31 +0545

    remove devlopment swager condition

--- Cafe.App/Program.cs
+++ Cafe.App/Program.cs
@@ -23,16 +23,14 @@ internal class Program
         });
 
         var app = builder.Build();
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
+      
             app.UseSwagger( options=>
             {
                 options.RouteTemplate = "swagger/{documentName}/swagger.json";
             });
 
             app.UseSwaggerUI();
-        }
+        
 
         app.UseHttpsRedirection();
 
